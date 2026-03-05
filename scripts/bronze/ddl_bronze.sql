dd9b092fa4762ed46e8c859e81165f26084102d8
Ananya Singh <88341482+ananyaSingh1305@users.noreply.github.com>
2026-03-05 18:32:40 +0530

    Create DDL script for bronze tables
    This script creates multiple tables in the 'bronze' schema, ensuring that existing tables are dropped before creation.

---
+++ scripts/bronze/ddl_bronze.sql
@@ -0,0 +1,74 @@
+/*
+===============================================================================
+DDL Script: Create Bronze Tables
+===============================================================================
+Script Purpose:
+    This script creates tables in the 'bronze' schema, dropping existing tables 
+    if they already exist.
+	  Run this script to re-define the DDL structure of 'bronze' Tables
+===============================================================================
+*/
+
+DROP TABLE IF EXISTS bronze_crm_cust_info;
+
+CREATE TABLE bronze_crm_cust_info(
+    cst_id INT,
+    cst_key VARCHAR(50),
+    cst_firstname VARCHAR(50),
+    cst_lastname VARCHAR(50),
+    cst_marital_status VARCHAR(50),
+    cst_gndr VARCHAR(50),
+    cst_create_date DATE
+);
+
+DROP TABLE IF EXISTS bronze_crm_cust_info;
+
+CREATE TABLE bronze_crm_cust_info(
+    prd_id INT,
+    prd_key VARCHAR(50),
+    prd_nm VARCHAR(50),
+    prd_cost VARCHAR(50),
+    prd_line VARCHAR(50),
+    prd_start_dt DATE,
+    prd_end_dt DATE
+);
+
+DROP TABLE IF EXISTS bronze_crm_cust_info;
+
+CREATE TABLE bronze_crm_cust_info(
+    sls_ord_num VARCHAR(50),
+    sls_prd_key VARCHAR(50),
+    sls_cust_id VARCHAR(50),
+    sls_order_dt  VARCHAR(50),
+    sls_ship_dt  VARCHAR(50),
+    sls_due_dt VARCHAR(50),
+    sls_sales VARCHAR(50),
+    sls_quantity VARCHAR(50),
+    sls_price  VARCHAR(50)
+);
+
+DROP TABLE IF EXISTS bronze_erp_cust_az12;
+
+CREATE TABLE bronze_erp_cust_az12(
+    CID VARCHAR(50),
+    BDATE DATE,
+    GEN VARCHAR(50)
+);
+
+DROP TABLE IF EXISTS bronze_erp_loc_a101;
+
+CREATE TABLE bronze_erp_loc_a101(
+    CID VARCHAR(50),
+    CNTRY VARCHAR(50)
+);
+
+DROP TABLE IF EXISTS bronze_erp_px_cat_g1v2;
+
+CREATE TABLE bronze_erp_px_cat_g1v2(
+    ID VARCHAR(50),
+    CAT VARCHAR(50),
+    SUBCAT VARCHAR(50),
+    MAINTENANCE VARCHAR(50)
+);
+
+/usr/local/mysql-8.0.36-macos14-x86_64/bin
